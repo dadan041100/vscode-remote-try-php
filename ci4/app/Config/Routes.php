f9420dec9eddf892acbbc7d5911fd5fe1b6ce4df
Daniel Ramolete <ddramolete@student.apc.edu.ph>
2024-02-22 17:04:23 +0800

    Add Guest to CI4

--- ci4/app/Config/Routes.php
+++ ci4/app/Config/Routes.php
@@ -9,9 +9,15 @@ $routes->get('/', 'Home::index');
 
 use App\Controllers\News;
 use App\Controllers\Pages;
-
+use App\Controllers\Guests;
 
 use App\Controllers\HomeController;
+
+
+$routes->get('guests', [Guests::class, 'index']);           
+$routes->get('guests/new', [Guests::class, 'new']); // Add this line
+$routes->post('guests', [Guests::class, 'create']); // Add this line
+$routes->get('guests/(:segment)', [Guests::class, 'show']); 
 
 $routes->get('news', [News::class, 'index']);
 $routes->get('news/new', [News::class, 'new']);
